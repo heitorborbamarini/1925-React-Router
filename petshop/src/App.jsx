d22f37eb77ad5848e39775fc03cfeb02c40470f5
heitorborbamarini <rm92976@fiap.com.br>
2022-11-07 09:17:07 -0300

    exibindo o post

--- petshop/src/App.jsx
+++ petshop/src/App.jsx
@@ -5,16 +5,19 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Home from './paginas/Home'
 import Sobre from './paginas/Sobre'
 import Pagina404 from './paginas/Pagina404'
+import Cabecalho from './components/Cabecalho'
+import Post from './paginas/Post'
 
 function App() {
 
   return (
     <Router>
+      <Cabecalho />
       <Switch>
         <Route exact path='/'>
           <Home />
         </Route>
-        <Route path='/sobre'>
+        <Route path='/posts/:id'>
           <Sobre />
         </Route>
         <Route>
